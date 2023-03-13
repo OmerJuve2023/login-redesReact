383096724a7ed338f44eda56213237ed1e8ebb84
OmerGit <rafaelxdale@gmail.com>
2023-03-13 09:55:07 -0500

    update to table

--- src/home/Register.js
+++ src/home/Register.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import "styles/styles.css";
+/*import "styles/styles.css";*/
 import AuthService from "../services/UserService";
 import {useNavigate} from "react-router-dom";
 
